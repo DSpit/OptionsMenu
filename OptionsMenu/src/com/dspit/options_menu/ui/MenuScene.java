c387dd3e5c309de0a810e07c3365336f36ff06ad
David Boivin ~Spit <db2_hockey@hotmial.com>
2014-11-13 21:56:05 -0500

    -Refactored the menu so that all the private UI components aren't layoing around in different files of code xP Something I should probably do a little more often

--- OptionsMenu/src/com/dspit/options_menu/ui/MenuScene.java
+++ OptionsMenu/src/com/dspit/options_menu/ui/MenuScene.java
@@ -8,12 +8,15 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 import com.dspit.options_menu.resources.ApplicationColourPalette;
 import com.dspit.options_menu.resources.ApplicationFormatting;
@@ -83,7 +86,7 @@ class MenuPane extends HBox{															//class MenuPane
 		this.setMenuOptions(controlHandler);
 	}
 	
-// Private ----------------------------------------------------------------- //
+// Private Members --------------------------------------------------------- //
 	
 	/**
 	 * Method which does the basic setup for each button in the Main
@@ -101,4 +104,79 @@ class MenuPane extends HBox{															//class MenuPane
 			this.getChildren().add(b);
 		}
 	}
+	
+// Private Inner Classes --------------------------------------------------- //
+	
+	class MenuButton extends Button {
+		
+		// Constants --------------------------------------------------------------- //
+			
+			private final double MIN_HEIGHT = ApplicationFormatting.MENU_BUTTON_SIZE.getHeight();
+			private final double MIN_WIDTH = ApplicationFormatting.MENU_BUTTON_SIZE.getWidth();
+			private final Background BACKGROUND = new Background( new BackgroundFill(
+					ApplicationColourPalette.COMPONENT_BACKGROUND, CornerRadii.EMPTY, Insets.EMPTY));
+			private final Background BACKGROUND_SELECTED = new Background( new BackgroundFill(
+					ApplicationColourPalette.ACCENT_BACKGROUND, CornerRadii.EMPTY, Insets.EMPTY));
+			private final Color FORGROUND = ApplicationColourPalette.COMPONENT_FORGROUND;
+			private final Font FONT =  ApplicationFormatting.BUTTON_FONT;
+
+		// Constructor ------------------------------------------------------------- //
+			
+			/**
+			 * Constructor which takes the name of the button and the handler which
+			 * takes care of handling the on click event.
+			 * 
+			 * @param text The text that will be appearing on the button.
+			 * @param handler The EventHandler which takes care of what happens to
+			 * 		this button when it gets clicked.
+			 */
+			public MenuButton(String text, EventHandler<ActionEvent> handler){
+				super(text);
+				
+				//set size formatting of the button (trust me, this thing shouldn't change size xP)
+				this.setPrefHeight(MIN_HEIGHT);
+				this.setPrefWidth(MIN_WIDTH);
+				this.setMinHeight(Control.USE_PREF_SIZE);
+				this.setMinWidth(Control.USE_PREF_SIZE);
+				this.setMaxHeight(Control.USE_PREF_SIZE);
+				this.setMaxWidth(Control.USE_PREF_SIZE);
+				
+				//set color formatting
+				this.setBackground(BACKGROUND);
+				this.setTextFill(FORGROUND);
+				
+				//set text formatting
+				this.setFont(FONT);
+				this.setWrapText(true);
+				
+				//set the given handler to the button
+				this.setOnAction(handler);
+				
+				//set visuals for the mouse click on the button (was broken by the setBackground() method.)
+				//To Teacher: If you want me to explain some more just ask me
+				EventHandler<MouseEvent> workaroundHandler = new VisualClickWorkaroundHandler();
+				this.setOnMousePressed(workaroundHandler);
+				this.setOnMouseReleased(workaroundHandler);
+			}
+			
+		//Private Listeners -------------------------------------------------------- //
+			
+			private class VisualClickWorkaroundHandler implements EventHandler<MouseEvent>{
+
+				@Override
+				public void handle(MouseEvent e) {
+					
+					//action to perform if the button is pressed
+					if(e.getEventType().equals(MouseEvent.MOUSE_PRESSED)){
+						setBackground(BACKGROUND_SELECTED);
+					}
+					
+					//action to perform if the button is released
+					if(e.getEventType().equals(MouseEvent.MOUSE_RELEASED)){
+						setBackground(BACKGROUND);
+					}
+				}
+				
+			}
+		}
 }
